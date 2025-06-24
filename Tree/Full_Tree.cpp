b1670fa2ef987c8310eedd967512495d8cc3bed1
TamaraMartikyan <tamaramartikyan04@gmail.com>
2025-06-24 14:14:32 +0400

    Update Full_Tree.cpp

--- Tree/Full_Tree.cpp
+++ Tree/Full_Tree.cpp
@@ -1,120 +1,118 @@
-#include<iostream>
+#include <iostream>
+#include <queue>
 using namespace std;
 
 class Node {
 public:
-	Node* left;
-	Node* right;
-	int data;
+    int data;
+    Node* left;
+    Node* right;
 
-	Node(int value) {
-		left = nullptr;
-		right = nullptr;
-		data = value;
-	}
-
+    Node(int value) {
+        data = value;
+        left = right = nullptr;
+    }
 };
 
 class Full_BT {
-private:
-	Node* root;
-
-	Node* insert(Node* node, int value) {
-		if (node == nullptr) {
-			return new Node(value);
-		}
-
-		if (node->right == nullptr) {
-			node->right = new Node(value);
-		}
-
-		else if (node->left == nullptr) {
-			node->right = new Node(value);
-		}
-		else {
-			if (node->left && node->right) {
-				node->right = insert(node->right, value);
-			}
-		}
-
-		return node;
-	}
-
-
-	bool is_full(Node* node) {
-		if (node == nullptr) {
-			return true;
-		}
-		if (node->left == nullptr && node->right == nullptr) {
-			return true;
-		}
-
-		if (node->left && node->right) {
-			return is_full(node->right) && is_full(node->left);
-		}
-
-		return false;
-	}
-
-
-	bool search(Node* node, int value) {
-
-		Node* curr = root;
-		while (curr) {
-			if (curr->data == value)
-				return true;
-		}
-		if (curr->data < value)
-			curr = curr->right;
-		else
-			curr = curr->left;
-	}
-
 public:
-	Full_BT() {
-		root = nullptr;
-	}
+    Node* root;
 
-	void insert(int value) {
-		if (root == nullptr) {
-			root = new Node(value);
-		}
-		else {
-			root = insert(root, value);
-		}
-	}
+    Full_BT() {
+        root = nullptr;
+    }
 
-	bool is_full() {
-		return is_full(root);
-	}
+    // Insert using level-order to maintain full binary tree
+    void insert(int value) {
+        if (root == nullptr) {
+            root = new Node(value);
+            return;
+        }
 
-	bool search(int value) {
-		return search(root, value);
-	}
+        queue<Node*> q;
+        q.push(root);
 
+        while (!q.empty()) {
+            Node* curr = q.front();
+            q.pop();
+
+            if (curr->left == nullptr) {
+                curr->left = new Node(value);
+                return;
+            } else if (curr->right == nullptr) {
+                curr->right = new Node(value);
+                return;
+            } else {
+                q.push(curr->left);
+                q.push(curr->right);
+            }
+        }
+    }
+
+    
+    bool is_full(Node* node) {
+        if (node == nullptr)
+            return true;
+
+        if (node->left == nullptr && node->right == nullptr)
+            return true;
+
+        if (node->left && node->right)
+            return is_full(node->left) && is_full(node->right);
+
+        return false;
+    }
+
+    
+    bool is_full() {
+        return is_full(root);
+    }
+
+    
+    bool search(Node* node, int value) {
+        if (node == nullptr) return false;
+
+        queue<Node*> q;
+        q.push(node);
+
+        while (!q.empty()) {
+            Node* curr = q.front();
+            q.pop();
+
+            if (curr->data == value)
+                return true;
+
+            if (curr->left) q.push(curr->left);
+            if (curr->right) q.push(curr->right);
+        }
+
+        return false;
+    }
+
+  
+    bool search(int value) {
+        return search(root, value);
+    }
 };
 
 int main() {
+    Full_BT fbt;
+    fbt.insert(10);
+    fbt.insert(8);
+    fbt.insert(15);
+    fbt.insert(100);
 
-	Full_BT fbt;
-	fbt.insert(10);
-	fbt.insert(8);
-	fbt.insert(15);
-	fbt.insert(100);
+    if (fbt.search(10)) {
+        cout << "Found" << endl;
+    } else {
+        cout << "Not found" << endl;
+    }
 
-	int value = 10;
-	if (fbt.search(10)) {
-		cout << "Found" << endl;
-	}
-	else
-		cout << "Not found" << endl;
+    if (fbt.is_full()) {
+        cout << "Tree is full" << endl;
+    } else {
+        cout << "Not full" << endl;
+    }
 
-	if (fbt.is_full()) {
-		cout << "Tree is full" << endl;
-	}
-	else cout << "Not full";
-
-
-
-	return 0;
+    return 0;
 }
